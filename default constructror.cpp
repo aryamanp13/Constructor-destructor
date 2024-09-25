2eb0ddf44d1bca9dfe2cf06a46e61b5819f7f914
Aryaman Pathak <aryamansp13@gmail.com>
2024-09-25 14:54:28 +0530

    Add files via upload

---
+++ default constructror.cpp
@@ -0,0 +1,31 @@
+//Aim:Implemention of Constructors and Destructors to perform Default Constructor
+
+#include <iostream>
+using namespace std;
+class construct{
+    public:
+    int a,b;
+
+construct(){
+    a = 10;
+    b = 20;
+}
+void display()
+{
+cout << "a = "<< a <<endl<< "b = "<<b;
+}
+};
+
+int main() {
+    construct constructor;
+    constructor.display();
+    
+
+    return 0;
+}
+
+
+/*Output:
+a = 10
+b = 20
+*/
